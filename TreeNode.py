d290caa76e718a51fcaa1e47f9439ce21d1d9433
Pablo <pablolostao@gmail.com>
2021-10-06 20:27:06 -0500

    Implementation changed to priority queue

--- TreeNode.py
+++ TreeNode.py
@@ -1,20 +1,17 @@
-from NeighborInfo import NeighborInfo
 from State import State
 
 
 class TreeNode:
     def __init__(self, state: State):
-        # State
         self.state = state
-        # Map state name to CostInfo
+        self.parent = None
+        self.cost = float("inf")
         self.neighbors = {}
 
     def __str__(self):
         res = "TreeNode of " + str(self.state) + ":\n"
-        for n in self.neighbors:
-            res += str(n)+": Driving: "+str(self.neighbors[n].drive_cost)+" Straight: "+str(self.neighbors[n].straight_cost)+"\n"
-        res+="\n"
+        if self.parent:
+            res += "Parent: "+str(self.parent.state.initials) + "\n"
+        res += "Cost: " + str(self.cost) + ":\n"
+        res += str(self.neighbors)
         return res
-
-    def add_neighbor(self, name: str, drive_cost: int, straight_cost: int):
-        self.neighbors[name] = (drive_cost, straight_cost)
